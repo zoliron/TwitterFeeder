a8da5e28f9d08473541aebee3944cf6ed94c94b6
noy ishai <noyishai1@gmail.com>
2019-02-18 13:05:28 +0200

    Or template

---
+++ src/LinkListener.java
@@ -0,0 +1,45 @@
+import il.ac.colman.cs.util.DataStorage;
+import il.ac.colman.cs.util.LinkExtractor;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class LinkListener {
+  public static void main(String[] args) throws SQLException {
+    // Connect to the database
+    DataStorage dataStorage = new DataStorage();
+
+    // Initiate our link extractor
+    LinkExtractor linkExtractor = new LinkExtractor();
+
+    // Listen to SQS for arriving links
+    // Configure our client
+    AmazonSQS client = AmazonSQSClientBuilder.defaultClient();
+// Send message to a Queue
+    client.sendMessage("https://sqs.us-east-1.amazonaws.com/135062767808/NoyIshai", "Our Message");
+
+    // Extract the link content
+    // ...
+
+    // Take screenshot
+    static String take(String url){
+      UUID uuid = UUID.randomUUID();
+      String filename = "screenshots\\" + uuid.toString() + ".png";
+      String[] cmd = { "node", "screenshot\\screenshot.js", url, filename };
+      try{
+        Process process = Runtime.getRuntime().exec(cmd);
+        process.waitFor();
+        return filename;
+      } catch (IOException e){
+        e.printStackTrace();
+      } catch (InterruptedException e){
+        e.printStackTrace();
+      }
+
+      return null;
+    }
+
+    // Save everything in the database
+  }
+}
